2b6373fd166a14ce912cca2fbb2d721a6866799c
pausanz <psppau@hotmail.com>
2017-03-22 18:02:10 +0100

    Obtinc els directors Selects, pero no afegeix peli

--- view/mostrar_selects.php
+++ view/mostrar_selects.php
@@ -1,14 +1,18 @@
 <?php
 
 //session_start();
-require_once("../controller/function_AutoLoad.php");
+require_once("controller/function_AutoLoad.php");
 
 function mostrarDirectors() {
     //if (isset($_SESSION['llibreria'])) {
     /*$llibreria = unserialize($_SESSION['llibreria']);
     $arrayAutors = $llibreria->getArraydeAutors();*/ //CONSULTA PDO DIRECTOR I GUARDAR
     //ARRAY
-    echo "<select name='director'>";
+    
+    $agencia = new agencia("ASDF");
+    $arrayDirectors = $agencia->rebreDirectors();
+    
+    echo "<select name='idDirector'>";
     foreach ($arrayDirectors as $director) {
         echo "<option value='" . $director->getNif() . "'>" . $director->getNom() . " " . $director->getCognom() . "</option>";
     }
