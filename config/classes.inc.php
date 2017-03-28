9cd150f46cca933912261f9cada7249b2f23ef02
pausanz <psppau@hotmail.com>
2017-03-28 16:26:47 +0200

    Class Validar a validar, nova formatació d'imatges

--- config/classes.inc.php
+++ config/classes.inc.php
@@ -1,39 +1,6 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	require_once("model/business/class_director.php");	
-	require_once("model/business/class_actor.php");	
-	require_once("model/business/class_paper.php");	
-	require_once("model/business/class_pelicula.php");
-	require_once("model/business/class_user.php");		
-=======
->>>>>>> master
-	require_once("model/business/class_actor.php");	
-	require_once("model/business/class_user.php");
-	require_once("model/business/class_paper.php");
-	require_once("model/business/class_director.php");
-	require_once("model/business/class_pelicula.php");	
-
-	//require_once("model/DAO/class_db.php");	
-<<<<<<< HEAD
-=======
->>>>>>> master
->>>>>>> master
-?>
-=======
-=======
-
->>>>>>> master
 require_once("model/business/class_director.php");
 require_once("model/business/class_actor.php");
 require_once("model/business/class_paper.php");
 require_once("model/business/class_pelicula.php");
 require_once("model/business/class_user.php");
-<<<<<<< HEAD
-
->>>>>>> master
-=======
->>>>>>> master
